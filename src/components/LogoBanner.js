f805ffea1c571fa2e7a9af8a9ddcbb92eac83bb7
ruthfeezota <ruthzota@gmail.com>
2023-07-26 13:32:01 -0500

    fixed animated banner UX

--- src/components/LogoBanner.js
+++ src/components/LogoBanner.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './LogoBanner.css'
+import Logo1 from '../Assets/comlogo1.png'
+import Logo2 from '../Assets/comlogo2.png'
+import Logo3 from '../Assets/comlogo3.png'
+import Logo4 from '../Assets/comlogo4.png'
+import Logo6 from '../Assets/comlogo6.png'
+import Logo7 from '../Assets/comlogo7.png'
+
 
 function LogoBanner() {
   return (
 <div id='scroll-container'>
     <div id="scroll-img">
-    <img src='https://i.ibb.co/gMqx5H6/8.png' alt='Logo'/>
+    <img src={Logo1} alt='Logo'/>
     </div>
     <div id="scroll-img">
-    <img src='https://i.ibb.co/2SQcLgD/9.png' alt='Logo'/>
+    <img src={Logo2} alt='Logo'/>
     </div>
     <div id="scroll-img">
-    <img src='https://i.ibb.co/sKcyb8T/10.png' alt='Logo'/>
+    <img src={Logo3} alt='Logo'/>
     </div>
     <div id="scroll-img">
-    <img src='https://i.ibb.co/xSTVgHD/11.png' alt='Logo'/>
+    <img src={Logo4} alt='Logo'/>
+    </div>
+    <div id="scroll-img">
+    <img src={Logo6} alt='Logo'/>
+    </div>
+    <div id="scroll-img">
+    <img src={Logo7} alt='Logo'/>
     </div>
 
 
